67e20f56a11c2a2ea190ee04e73282ab7e5d9740
Rohit <39988015+rohetoric@users.noreply.github.com>
2024-01-15 20:46:13 +0530

    Time: 41 ms (47.48%) | Memory: 17.3 MB (27.69%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,20 +1,41 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x >= 0:
-            x = str(x)
-            x = x[::-1]
-            x = int(x)
-            if x >2**31 or x < -2 **31:
+
+        MAX_INT = 2**31
+        MIN_INT = -2**31
+        reverse = 0
+
+        while x != 0:
+            if x >= 0:
+                last_digit = x % 10
+            else:
+                last_digit = x % -10
+            
+            reverse = reverse*10 + last_digit
+            if reverse > 2**31 or reverse <-2**31:
                 return 0
-            return x
-        else:
-            x = str(x)
-            sign = x[0]
-            x = x[1:]
-            x = x[::-1]
-            x = -1 * int(x)
-            if x >2**31 or x < -2 **31:
-                return 0
-            return x
+            x = math.trunc(x/10)
+
+        return reverse
+
+
+
+
+        # if x >= 0:
+        #     x = str(x)
+        #     x = x[::-1]
+        #     x = int(x)
+        #     if x >2**31 or x < -2 **31:
+        #         return 0
+        #     return x
+        # else:
+        #     x = str(x)
+        #     sign = x[0]
+        #     x = x[1:]
+        #     x = x[::-1]
+        #     x = -1 * int(x)
+        #     if x >2**31 or x < -2 **31:
+        #         return 0
+        #     return x
 
         
